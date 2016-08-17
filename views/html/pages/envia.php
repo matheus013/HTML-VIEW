fc333616350cf5cb3e67f6f1d40a17c322a7399c
matheus-dev <matheus.bs@ic.ufal.br>
2016-08-17 18:16:18 -0300

    update

--- views/html/pages/envia.php
+++ views/html/pages/envia.php
@@ -1,28 +1,25 @@
 <?php
 
-// Passando os dados obtidos pelo formulário para as variáveis abaixo
-$nomeremetente     = $_POST['senderName'];
-$emailremetente    = trim($_POST['senderEmail']);
-$emaildestinatario = 'matheusinacio.bs@gmail.com'; // Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
-$assunto          = $_POST['subject'];
-$mensagem          = $_POST['message'];
- 
- 
-/* Montando a mensagem a ser enviada no corpo do e-mail. */
-$mensagemHTML = '<P>Formulario do site.</P>
-<p><b>Nome:</b> '.$nomeremetente.'
-<p><b>E-Mail:</b> '.$emailremetente.'
-<p><b>Assunto:</b> '.$assunto.'
-<p><b>Mensagem:</b> '.$mensagem.'</p>
+$senderName = $_POST['senderName'];
+$senderEmail = trim($_POST['senderEmail']);
+$receiverEmail = 'matheusinacio.bs@gmail.com'; // Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$messageHTML = '<P>Formulario do site.</P>
+<p><b>Nome:</b> ' . $senderName . '
+<p><b>E-Mail:</b> ' . $senderEmail . '
+<p><b>Assunto:</b> ' . $subject . '
+<p><b>Mensagem:</b> ' . $message . '</p>
 <hr>';
-// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
-// O return-path deve ser ser o mesmo e-mail do remetente.
+
 $headers = "MIME-Version: 1.1\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
-$headers .= "From: $emailremetente\r\n"; // remetente
-$headers .= "Return-Path: $emaildestinatario \r\n"; // return-path
-$envio = mail($emaildestinatario, $assunto, $mensagemHTML, $headers); 
- 
- if($envio)
+$headers .= "From: $senderEmail\r\n"; // remetente
+$headers .= "Return-Path: $receiverEmail \r\n"; // return-path
+$envio = mail($receiverEmail, $subject, $messageHTML, $headers);
+
+if ($envio)
     echo "<script>location.href='http://fb.com/'</script>"; // Página que será redirecionada
 ?>
